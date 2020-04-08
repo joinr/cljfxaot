42e2e4eb114d970eee277b395e3297d42a5a09e1
tom <tom@that-place>
2020-04-08 07:52:31 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljfxaot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/cljfxaot/core.clj
@@ -0,0 +1,8 @@
+(ns cljfxaot.core
+  (:require [cljfx.api :as fx])
+  (:gen-class :main true))
+
+(defn -main
+  "I don't do a whole lot."
+  []
+  (println "Hello, World!"))
